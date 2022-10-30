2748576f6c618b16b4e572a31b29042125e7417a
Coker-J <117033815+Coker-J@users.noreply.github.com>
2022-10-31 01:21:34 +0800

    Create acme.sh

---
+++ acme.sh
@@ -0,0 +1,4 @@
+apt install socat;curl https://get.acme.sh | sh -s email=123456@qq.com
+source ~/.bashrc
+read -p "输入你的域名" domain
+acme.sh --issue --standalone -d $domain || acme.sh --issue --standalone -d $domain --server letsencrypt
